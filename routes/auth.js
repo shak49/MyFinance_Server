fc402fae0bcddf622ff9883e174f0b2483821f6f
Shak Feizi <>
2024-09-14 22:51:37 -0500

    commented temporary

--- routes/auth.js
+++ routes/auth.js
@@ -58,56 +58,56 @@ router.get('/auth/sign-in', async (req, res) => {
 });
 // Apple authentication
 router.post('/auth/apple', async (req, res) => {
-    const { token, email, apple_id } = req.body;
-    const registeredUser = { apple_id, email };
-    const user = await User.findOne({ apple_id: apple_id });
-    if (user) {
-        if (email && email !== user.email) {
-            User.updateOne(req.body);
-            user.email = req.body.email;
-        }
-        res.status(200).send(user);
-    } else {
-        await axios.get(env.APPLE_PUBLIC_KEYS, async (error, body) => {
-            if (error) {
-                res.status(401).json({ error: error });
-            } else {
-                //const key = jwt.asKeyStore(body);
-                try {
-                    const verified = jwt.verify(token, 'secret');
-                    if (verified) await user.save(registeredUser);
-                } catch(error) {
-                    res.status(500).json({ error: error });
-                }
-            }
-        });
-    }
+    // const { token, email, apple_id } = req.body;
+    // const registeredUser = { apple_id, email };
+    // const user = await User.findOne({ apple_id: apple_id });
+    // if (user) {
+    //     if (email && email !== user.email) {
+    //         User.updateOne(req.body);
+    //         user.email = req.body.email;
+    //     }
+    //     res.status(200).send(user);
+    // } else {
+    //     await axios.get(env.APPLE_PUBLIC_KEYS, async (error, body) => {
+    //         if (error) {
+    //             res.status(401).json({ error: error });
+    //         } else {
+    //             //const key = jwt.asKeyStore(body);
+    //             try {
+    //                 const verified = jwt.verify(token, 'secret');
+    //                 if (verified) await user.save(registeredUser);
+    //             } catch(error) {
+    //                 res.status(500).json({ error: error });
+    //             }
+    //         }
+    //     });
+    // }
 });
 // Google sign in request
 router.get('/auth/google/sign-in', (req, res) => {
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${env.CLIENT_ID}&redirect_uri=${env.REDIRECT_URI}&response_type=code&scope=profile email`;
-    res.redirect(url);
+    // const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${env.CLIENT_ID}&redirect_uri=${env.REDIRECT_URI}&response_type=code&scope=profile email`;
+    // res.redirect(url);
 });
 // Google sign in callback
 router.get('/auth/google/sign-in/callback', async (req, res) => {
-    const { code } = req.query;
-    try {
-        const { data } = await axios.post('<https://oauth2.googleapis.com/token>', {
-            client_id: env.CLIENT_ID,
-            client_secret: env.CLIENT_SECRET,
-            code,
-            redirect_uri: env.REDIRECT_URI,
-            grant_type: 'authorization_code',
-        });
-        const { access_token, id_token } = data;
-        const profile = await axios.get('<https://www.googleapis.com/oauth2/v1/userinfo>', {
-            headers: { Authorization: `Bearer ${access_token}` },
-        });
-        res.status(200).json(profile);
-    } catch(error) {
-        res.status(501).json({ error: 'Internal server error.'});
-        res.redirect('/login');
-    }
+    // const { code } = req.query;
+    // try {
+    //     const { data } = await axios.post('<https://oauth2.googleapis.com/token>', {
+    //         client_id: env.CLIENT_ID,
+    //         client_secret: env.CLIENT_SECRET,
+    //         code,
+    //         redirect_uri: env.REDIRECT_URI,
+    //         grant_type: 'authorization_code',
+    //     });
+    //     const { access_token, id_token } = data;
+    //     const profile = await axios.get('<https://www.googleapis.com/oauth2/v1/userinfo>', {
+    //         headers: { Authorization: `Bearer ${access_token}` },
+    //     });
+    //     res.status(200).json(profile);
+    // } catch(error) {
+    //     res.status(501).json({ error: 'Internal server error.'});
+    //     res.redirect('/login');
+    // }
 });
 // Sign Out
 router.get('/auth/sign-out', (req, res) => {
@@ -118,13 +118,13 @@ router.get('/auth/sign-out', (req, res) => {
 });
 // Current User
 router.get('/auth/current-user', (req, res) => {
-    const token = req.cookies.jwt;
-    if (!token) return res.status(401).json({ error: 'Invalid token.' });
-    jwt.verify(token, 'secret', (error, decodedToken) => {
-        if (error) return res.status(401).json(error);
-        const user = User.findById(decodedToken.id);
-        res.status(200).json(user);
-    });
+    // const token = req.cookies.jwt;
+    // if (!token) return res.status(401).json({ error: 'Invalid token.' });
+    // jwt.verify(token, 'secret', (error, decodedToken) => {
+    //     if (error) return res.status(401).json(error);
+    //     const user = User.findById(decodedToken.id);
+    //     res.status(200).json(user);
+    // });
 });
 
 export default router;

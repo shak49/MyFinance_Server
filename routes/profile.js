afe73ae2a4faf1699de1ab3c35ce25eadb308fc3
Shak Feizi <>
2024-09-24 22:06:26 -0500

    updated

--- routes/profile.js
+++ routes/profile.js
@@ -1,20 +1,20 @@
 //
 //
 //
-import express from 'express';
+import express, { response } from 'express';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 const router = express.Router();
 const env = process.env;
 // Current User
-router.get('/profile/current-user', (req, res, next) => {
+router.get('/profile/current-user', (req, res) => {
     const token = req.cookies.token;
     if (!token) return res.status(401).json({ error: 'Invalid token.' });
     try {
         const decoded = jwt.verify(token, 'secret');
-        req.user = decoded.user;
-        next();
+        const user = decoded.user;
+        res.status(200).json(user);
     } catch (error) {
         return res.status(401).json(error);
     }

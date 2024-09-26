4237ec7b4e2d7de25defe4ebfa3ef6b0c74c0d02
Shak Feizi <>
2024-09-26 18:18:48 -0500

    current-user implemented

--- routes/profile.js
+++ routes/profile.js
@@ -8,12 +8,12 @@ import User from '../models/User.js';
 const router = express.Router();
 const env = process.env;
 // Current User
-router.get('/profile/current-user', (req, res) => {
+router.get('/profile/current-user', async (req, res) => {
     const token = req.headers.authorization;
     if (!token) return res.status(401).json({ error: 'Invalid token.' });
     try {
         const decoded = jwt.verify(token, 'secret');
-        const user = decoded.user;
+        const user = await User.findOne({ email: decoded.email });
         res.status(200).json(user);
     } catch (error) {
         return res.status(401).json(error);
